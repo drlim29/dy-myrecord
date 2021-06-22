0f9ca1a61061a5be50948baf41fdd7688c1c5d2d
kjo7434 <jo7434@naver.com>
2021-06-22 11:21:46 +0900

    kjo7434_fin

--- routes/voice.js
+++ routes/voice.js
@@ -1,57 +1,81 @@
+//express , router 사용
 const express = require('express');
 const router = express.Router();
 
-//db
+//db 연결
 const path = require('path');
 const db_config = require('../config/database.js');
 const conn = db_config.init();
 db_config.connect(conn);
 
-//녹음파일 리스트 출력
+//녹음파일 리스트 /voice/
 router.get('/', function (req, res) {
   let sessInfo = {};
+
+  //사용자 고유값 읽기
   if (typeof req.user !== "undefined") {
     sessInfo = parseSession(req.user);
   }
-  
+
+  //Query & Parameter setting
   let sqlQuery="SELECT * FROM content_voice WHERE account_seq=?;"
-  paramsSelect=[sessInfo.account_seq]
-  
+  paramsSelect=[sessInfo.account_id]
+
   conn.query(sqlQuery, paramsSelect, (err, result, fields) => {
-    if(sessInfo.account_seq !== undefined) {
+    if(sessInfo.account_id !== undefined) {
       console.log("-----Voice ContentList Page Load-----")
       res.render('voiceContentList', {sess: sessInfo, data: result});
     }
     else {
       res.render("login",{sess: sessInfo})
     }
-});
-/*
-  let sessInfo = {};
-  if (typeof req.user !== "undefined") {
-    sessInfo = parseSession(req.user);
-  }
-  res.render('testam', {sess:sessInfo});
-  // res.render('testam.ejs');
-  */
+  });
+
 })
 
+//데이터베이스 삽입
+router.get('/recordInsert', function (req, res) {
+  let {voice, title, content} = req.query
 
-//녹음파일 세부정보 출력
-router.get('/voiceContentInfo/:content_seq', function (req, res) {
+  //사용자 고유값 읽기
   let sessInfo = {};
-  let content_seq = req.params.content_seq;
-  
   if (typeof req.user !== "undefined") {
-    sessInfo = parseSession(req.user);
+    sessInfo = parseSession(req.user);                    
   }
-  //유저정보 불러오는 쿼리 사용
-  //const sqlQuery="SELECT * FROM content_voice WHERE " 유저 테이블 생성 시 사용
-  //임시 쿼리
-  sqlQuery="SELECT * FROM content_voice WHERE content_seq=?"
-  paramsSelect=[content_seq]
-  console.log(sqlQuery)
-  console.log(paramsSelect)
+
+  //현재시간
+  let today = new Date()
+  let year = today.getFullYear();                         // 년도
+  let month = today.getMonth() + 1;                       // 월
+  let date = today.getDate()
+  let reporting_date=year + '/' + month + '/' + date
+
+  //Query setting
+  let sqlQuery="INSERT INTO content_voice(account_seq, title, content, voice, reporting_date) VALUE (?,?,?,?,?)"
+  let paramsSelect = [sessInfo.account_id, title, content, voice, reporting_date];
+  //Query
+  conn.query(sqlQuery, paramsSelect, (err, results, fields) => {
+    if (error) {
+      console.log("-----Voice Content insert-----")
+    }
+    console.log("-----Voice Content insert: Failed-----")
+  });
+  res.redirect("/voice");
+})
+
+//녹음파일 세부정보
+router.get('/voiceContentInfo/:content_seq', function (req, res) {
+  let content_seq = req.params.content_seq;
+  let sessInfo = {};
+
+  //사용자 고유값 읽기
+  if (typeof req.user !== "undefined") {
+    sessInfo = parseSession(req.user);                    
+  }
+
+  //Query & Parameter setting
+  const sqlQuery="SELECT * FROM content_voice WHERE content_seq=?"
+  const paramsSelect=[content_seq]
   conn.query(sqlQuery, paramsSelect, (err, result, fields) => {
       if(err) throw err;
       else{
@@ -60,6 +84,7 @@ router.get('/voiceContentInfo/:content_seq', function (req, res) {
           res.render('voiceContentInfo', {title: "Voice ContentInfo", data: result , sess: sessInfo});
         }
         else {
+          console.log("-----Voice ContentInfo Page Load: Failed-----")
           res.redirect("/voice");
         }
       }
@@ -69,48 +94,46 @@ router.get('/voiceContentInfo/:content_seq', function (req, res) {
 
 //컨텐츠 수정
 router.get('/ContentInfoUpdate/', function (req, res) {
-  //Get Parameter
   let {content_seq, title, content} = req.query
-  //Get Session
   let sessInfo = {};
+  
+  //사용자 고유값 읽기
   if (typeof req.user !== "undefined") {
     sessInfo = parseSession(req.user);
-  }  
+  }
 
-  //Query setting
+  //Query & Parameter setting
   let sqlQuery="UPDATE content_voice SET title=?, content=? WHERE content_seq=?"
-  //Parameter setting
   let paramsSelect = [title, content, content_seq];
 
-  console.log(sqlQuery)
   conn.query(sqlQuery, paramsSelect, (err, result, fields) => {
       if(err) throw err;
       else{
         if(req.user != "undefined") {
           console.log("-----Voice ContentInfo Update-----")
-          res.redirect("/voice");
         }
         else {
-          res.redirect("/voice");
+          console.log("-----Voice ContentInfo Update: Failed-----")
         }
+        res.redirect("/voice");
       }
   })
 });
 
 //컨텐츠 삭제
-router.post('/ContentInfoDelete/:content_seq', function (req, res) {
+router.get('/ContentInfoDelete/:content_seq', function (req, res) {
+  let {content_seq} = req.query;
   let sessInfo = {};
+
+  //사용자 고유값 읽기
   if (typeof req.user !== "undefined") {
-    sessInfo = parseSession(req.user);
+    sessInfo = parseSession(req.user);                    //사용자 고유값 읽기
   }
-  let content_seq = req.params.content_seq;
-
-
-  //Query setting
+  
+  //Query & Parameter setting
   let sqlQuery="DELETE FROM content_voice WHERE content_seq=?"
-  //Parameter setting
   let paramsSelect = [content_seq];
-  console.log(sqlQuery)
+  //Query
   conn.query(sqlQuery, paramsSelect, (err, result, fields) => {
       if(err) throw err;
       else{
@@ -119,83 +142,29 @@ router.post('/ContentInfoDelete/:content_seq', function (req, res) {
           res.redirect("/voice");
         }
         else {
-          alert("오류가 발생했습니다.")
+          console.log("-----Voice Content delete: Failed-----")
           res.redirect("/voice")
         }
       }
-
   })
 })
 
 
 
-//컨텐츠 생성페이지
+//녹음 페이지
 router.get('/record/:account_seq', function (req, res) {
   let sessInfo = {};
+
+  //사용자 고유값 읽기
   if (typeof req.user !== "undefined") {
-    sessInfo = parseSession(req.user);
+    sessInfo = parseSession(req.user);                    
   }
   let account_seq = req.params.account_seq;
-  //유저정보 불러오는 쿼리 사용
-  //const sqlQuery="SELECT * FROM content_voice WHERE " 유저 테이블 생성 시 사용
-  //임시 쿼리
-  // sqlQuery="SELECT * FROM content_voice WHERE content_seq=?"
-  // paramsSelect=[content_seq]
-  // console.log(sqlQuery)
-  // console.log(paramsSelect)
-  // conn.query(sqlQuery, paramsSelect, (err, result, fields) => {
-  //     if(err) throw err;
-  //     else{
-  //       if(req.user != "undefined") {
-  //         console.log("-----Voice ContentInfo Page Load-----")
-  //         res.render('voiceContentInfo', {title: "Temporary Title", data: result , sess: sessInfo});
-  //       }
-  //       else {
-  //         res.redirect("/login")
-  //       }
-  //     }
-
-  // })
-
-
   res.render('voiceRecord', {data: account_seq , sess:sessInfo});
-
-
 })
 
 
-//데이터베이스 삽입
-router.get('/recordInsert', function (req, res) {
-  //Get Parameter
-  let {account_seq, voice, title, content} = req.query
-  //session Setting
-  let sessInfo = {};
-  if (typeof req.user !== "undefined") {
-    sessInfo = parseSession(req.user);
-  }
 
-  //입력시간
-  let today = new Date()
-  let year = today.getFullYear(); // 년도
-  let month = today.getMonth() + 1;  // 월
-  let date = today.getDate()
-  let reporting_date=year + '/' + month + '/' + date
-  console.log("Now date : "+reporting_date)
-
-
-  //Query setting
-  let sqlQuery="INSERT INTO content_voice(account_seq, title, content, voice, reporting_date) VALUE (?,?,?,?,?)"
-  let paramsSelect = [account_seq, title, content, voice, reporting_date];
-  
-  conn.query(sqlQuery, paramsSelect, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    }
-    console.log(results);
-  });
-  
-  res.redirect("/voice");
-})
 
 //세션 파싱
 function parseSession(sess)
